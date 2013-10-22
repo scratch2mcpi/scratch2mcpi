efe3bedb76ac2bef016791b401b41b5056e1e1a0
Junya Ishihara <junya@champierre.com>
2013-10-22 20:45:10 +0900

    if minecraft pi cannot be found, just guide user to manually copy mcpi folder.

--- install.sh
+++ install.sh
@@ -3,13 +3,10 @@ wget -P /tmp https://github.com/champierre/scratch2minecraft/archive/master.zip
 unzip /tmp/master.zip
 mv scratch2minecraft-master scratch2minecraft
 rm /tmp/master.zip
-echo -n "Please specify Minecraft Pi installation directory[/home/pi/mcpi]: "
-read dir 
-case $dir in
-  /*)
-    cp -r $dir/api/python/mcpi scratch2minecraft/;;
-  *)
-    cp -r /home/pi/mcpi/api/python/mcpi scratch2minecraft/;;
-esac
+if test -d /home/pi/mcpi/api/python/mcpi; then
+  cp -r /home/pi/mcpi/api/python/mcpi scratch2minecraft/
+else
+  echo "Unable to copy 'mcpi' directory to 'scratch2minecraft'. You should manually copy '[minecraft pi dir]/api/python/mcpi' to 'scratch2minecraft'."
+fi
 echo "Installation of scratch2minecraft is completed."
 
