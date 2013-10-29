c07cd46662f9d7d358c4cc8804ec56e5b004579e
Junya Ishihara <junya@champierre.com>
2013-10-29 16:52:23 +0000

    modified scratch project file path

--- scratch2mcpi.sh
+++ scratch2mcpi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /home/pi/mcpi/minecraft-pi &
 ps aux | grep 'python.*scratch2mcpi.py' | grep -v grep | awk '{print $2}' | xargs sudo kill -9 
-scratch --document /home/pi/scratch2mcpi/hi.sb &
+scratch --document "/home/pi/Documents/Scratch Projects/hello_minecraft.sb" &
 sleep 20
 python /home/pi/scratch2mcpi/scratch2mcpi.py &
