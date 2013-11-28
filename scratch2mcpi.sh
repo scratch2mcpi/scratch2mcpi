764df811d839bc9a47d523e7d2a5b8d5fb89a535
Junya Ishihara <junya@champierre.com>
2013-11-28 11:30:40 +0000

    add title to the terminal window

--- scratch2mcpi.sh
+++ scratch2mcpi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo ps aux | grep 'scratch2mcpi.py' | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 scratch --document "/home/pi/Documents/Scratch Projects/mcpi_template.sb" &
-sleep 40
-lxterminal -e python /home/pi/scratch2mcpi/scratch2mcpi.py
+sleep 30
+lxterminal -t Scratch2MCPI -e python /home/pi/scratch2mcpi/scratch2mcpi.py
 
--- scratch2mcpi_terminal.sh
+++ scratch2mcpi_terminal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo ps aux | grep 'scratch2mcpi.py' | grep -v grep | awk '{print $2}' | xargs sudo kill -9
-lxterminal -e python /home/pi/scratch2mcpi/scratch2mcpi.py
+lxterminal -t Scratch2MCPI -e python /home/pi/scratch2mcpi/scratch2mcpi.py
 
