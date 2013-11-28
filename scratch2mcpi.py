3d4ab2368cd773a61ad8bdfc6c99fd69b85b28b6
Junya Ishihara <junya@champierre.com>
2013-11-28 10:10:05 +0000

    improve reset the world

--- scratch2mcpi.py
+++ scratch2mcpi.py
@@ -97,6 +97,7 @@ for msg in listen():
     elif msg[1] == 'reset':
       mc.postToChat('reset the world')
       mc.setBlocks(-100, 0, -100, 100, 63, 100, 0, 0)
+      mc.setBlocks(-100, -63, -100, 100, -2, 100, 1, 0)
       mc.setBlocks(-100, -1, -100, 100, -1, 100, 2, 0)
       mc.player.setPos(0, 0, 0)
   elif msg[0] == 'sensor-update':
--- scratch2mcpi.sh
+++ scratch2mcpi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo ps aux | grep 'scratch2mcpi.py' | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 scratch --document "/home/pi/Documents/Scratch Projects/mcpi_template.sb" &
-sleep 30
+sleep 40
 lxterminal -e python /home/pi/scratch2mcpi/scratch2mcpi.py
 
