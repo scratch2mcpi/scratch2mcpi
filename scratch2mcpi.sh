7623843c7856f915828f8d13cf597a7db65ff36e
Junya Ishihara <junya@champierre.com>
2014-12-27 21:28:04 +0900

    make the script work with NuScratch

--- scratch2mcpi.sh
+++ scratch2mcpi.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 sudo ps aux | grep 'scratch2mcpi.py' | grep -v grep | awk '{print $2}' | xargs sudo kill -9
-scratch --document "/home/pi/Documents/Scratch Projects/mcpi_template.sb" &
-sleep 30
+NU_SCRATCH=`ls /usr/share/scratch/NuScratch*.image`
+MCPI_TEMPLATE="/home/pi/Documents/Scratch Projects/mcpi_template.sb"
+if [ $NU_SCRATCH ]
+then
+  /usr/bin/squeak-stack -vm-sound-alsa $NU_SCRATCH --document $MCPI_TEMPLATE & sleep 60
+else
+  scratch --document $MCPI_TEMPLATE & sleep 30
+fi
 lxterminal -t Scratch2MCPI -e python /home/pi/scratch2mcpi/scratch2mcpi.py
-
