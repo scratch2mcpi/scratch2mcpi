f6741fe627272bb83779e2719950b218f0050d92
Junya Ishihara <junya@champierre.com>
2014-12-27 21:38:37 +0900

    fix error in shell script

--- scratch2mcpi.sh
+++ scratch2mcpi.sh
@@ -4,8 +4,8 @@ NU_SCRATCH=`ls /usr/share/scratch/NuScratch*.image`
 MCPI_TEMPLATE="/home/pi/Documents/Scratch Projects/mcpi_template.sb"
 if [ $NU_SCRATCH ]
 then
-  /usr/bin/squeak-stack -vm-sound-alsa $NU_SCRATCH --document $MCPI_TEMPLATE & sleep 60
+  /usr/bin/squeak-stack -vm-sound-alsa $NU_SCRATCH --document "$MCPI_TEMPLATE" & sleep 60
 else
-  scratch --document $MCPI_TEMPLATE & sleep 30
+  scratch --document "$MCPI_TEMPLATE" & sleep 30
 fi
 lxterminal -t Scratch2MCPI -e python /home/pi/scratch2mcpi/scratch2mcpi.py
