89aa4a6fd06cff912a742f2ee78848651cf194b7
Junya Ishihara <junya@champierre.com>
2015-10-25 22:33:11 +0900

    Fix the bug: scratch2mcpi now starts from script on 2015-09-24-raspbian-jessie.

--- scratch2mcpi.sh
+++ scratch2mcpi.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 sudo ps aux | grep 'scratch2mcpi.py' | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 NU_SCRATCH=`ls /usr/share/scratch/NuScratch*.image`
+SQUEAK_STACK=`ls /usr/bin/squeak-stack`
 MCPI_TEMPLATE="/home/pi/Documents/Scratch Projects/mcpi_template.sb"
-if [ $NU_SCRATCH ]
+if [ $NU_SCRATCH ] -a [ $SQUEAK_STACK ]
 then
   /usr/bin/squeak-stack -vm-sound-alsa $NU_SCRATCH --document "$MCPI_TEMPLATE" & sleep 60
 else
